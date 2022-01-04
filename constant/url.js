89e905e346fb9a4903fe0f5cba8345832bf98446
= <bbadrunn@gmail.com>
2022-01-04 08:18:58 +0700

    update

--- constant/url.js
+++ constant/url.js
@@ -1,3 +1,4 @@
 // export default process.env.NODE_ENV !== 'production' ? process.env.NEXT_PUBLIC_URL : process.env.VERCEL_URL
 
-export default "http://localhost:3000/api"
+// export default "http://localhost:3000/api"
+export default "blogger-me-qfwwrsdbi-badrunp.vercel.app"
